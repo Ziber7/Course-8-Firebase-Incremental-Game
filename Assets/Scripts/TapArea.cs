3889be35aa2445c3c7ba78b2e748a1c5b040f819
Ziber7 <naufaljundi.2019@student.uny.ac.id>
2021-10-02 03:34:59 +0700

    push step 1

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float volume;
+
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(clip, volume);
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+    }
+}
